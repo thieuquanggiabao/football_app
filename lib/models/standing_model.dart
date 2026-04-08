f1b09a80608ab4ece4ab354515815a0dc0b9d8aa
HieuNguyen243 <hieunguyen0816470@gmail.com>
2026-04-08 17:17:39 +0700

    add lost,win, draw column in standing model

--- lib/models/standing_model.dart
+++ lib/models/standing_model.dart
@@ -3,6 +3,9 @@ class StandingModel {
   final String teamName;
   final String teamLogo;
   final int played;
+  final int won;
+  final int drawn;
+  final int lost;
   final int goalDifference;
   final int points;
 
@@ -11,6 +14,9 @@ class StandingModel {
     required this.teamName,
     required this.teamLogo,
     required this.played,
+    required this.won,
+    required this.drawn,
+    required this.lost,
     required this.goalDifference,
     required this.points,
   });
@@ -21,6 +27,9 @@ class StandingModel {
       teamName: json['team_name'] ?? 'Đang cập nhật',
       teamLogo: json['team_logo'] ?? '',
       played: json['played'] ?? 0,
+      won: json['won'] ?? 0,
+      drawn: json['drawn'] ?? 0,
+      lost: json['lost'] ?? 0,
       goalDifference: json['goal_difference'] ?? 0,
       points: json['points'] ?? 0,
     );
